6beadbb281a5448ff749d6632fc17cedb53d6b8e
Nada <nadak4171@gmail.com>
2024-08-15 01:24:48 +0300

    task#28+#29+#30

--- Tasks/Task28.js
+++ Tasks/Task28.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {
   Pressable,
   SafeAreaView,
@@ -6,6 +6,10 @@ import {
   Image,
   StyleSheet,
   Alert,
+  View,
+  Text,
+  Button,
+  TextInput,
 } from 'react-native';
 import CatPic from '../Resources/Cat.webp';
 import EarthPic from '../Resources/Earth.jpg';
@@ -19,7 +23,7 @@ import space from '../Resources/space.jpg';
 import tree from '../Resources/Tree.webp';
 
 export default function Task28() {
-  const [Picture] = useState([
+  const [Picture, setPictures] = useState([
     {name: CatPic, key: '1'},
     {name: EarthPic, key: '2'},
     {name: DogPic, key: '3'},
@@ -31,23 +35,88 @@ export default function Task28() {
     {name: space, key: '9'},
     {name: tree, key: '10'},
   ]);
-  function PressImage(keys) {
-    Alert.alert(`You have selected image: ${keys}`);
+  const [textIndex, setTextIndex] = useState('');
+  const FlatListRef = useRef(null);
+  const TextInputRef = useRef(null);
+  function PressImage(index) {
+    Alert.alert(`You have selected image: ${index}`);
+  }
+  function RemovePicture(key) {
+    setPictures(prevPictures => prevPictures.filter(item => item.key !== key));
+  }
+  function InputIndex(index) {
+    if (FlatListRef.current && index >= 0 && index < Picture.length) {
+      FlatListRef.current.scrollToIndex({
+        animated: true,
+        index: parseInt(index),
+      });
+    } else {
+      Alert.alert(
+        'Invalid Index',
+        'Please enter a valid index between 0 and 9',
+      );
+    }
+  }
+  function handleButtonPress() {
+    if (textIndex === '') {
+      Alert.alert(
+        'Input Required',
+        'Please enter an index to show the picture.',
+        [
+          {
+            text: 'OK',
+            onPress: () => TextInputRef.current.focus(),
+          },
+        ],
+      );
+    } else {
+      Alert.alert(
+        'Image Index',
+        'Please Input Index between 0-9 To Show Picture ',
+        [
+          {
+            text: 'Cancel',
+            style: 'cancel',
+          },
+          {text: 'OK', onPress: () => InputIndex(textIndex)},
+        ],
+      );
+    }
   }
   return (
     <SafeAreaView>
-      <FlatList
-        horizontal={true}
-        data={Picture}
-        renderItem={({item}) => (
-          <Pressable
-            onPress={() => {
-              PressImage(item.key);
-            }}>
-            <Image source={item.name} style={styles.image} />
-          </Pressable>
-        )}
-      />
+      <View>
+        <FlatList
+          ref={FlatListRef}
+          horizontal={true}
+          data={Picture}
+          renderItem={({item, index}) => (
+            <View>
+              <Pressable
+                onPress={() => {
+                  PressImage(index);
+                }}>
+                <Image source={item.name} style={styles.image} />
+              </Pressable>
+              <Pressable
+                onPress={() => RemovePicture(item.key)}
+                style={styles.icon}>
+                <Text>Remove</Text>
+              </Pressable>
+            </View>
+          )}
+        />
+        <Button title="Show Image" onPress={handleButtonPress} />
+        <TextInput
+          ref={TextInputRef}
+          style={styles.input}
+          placeholder="Insert Index Of Image..."
+          placeholderTextColor="#999"
+          keyboardType="numeric"
+          onChangeText={setTextIndex}
+          value={textIndex}
+        />
+      </View>
     </SafeAreaView>
   );
 }
@@ -59,5 +128,34 @@ const styles = StyleSheet.create({
     marginRight: 10,
     borderRadius: 10,
     resizeMode: 'cover',
+    elevation: 5,
+  },
+  icon: {
+    marginTop: 250,
+    position: 'absolute',
+    top: 5,
+    right: 5,
+    backgroundColor: 'rgba(255,20,147,0.8)',
+    borderRadius: 10,
+    padding: 5,
+    zIndex: 1,
+    elevation: 5,
+  },
+  input: {
+    fontSize: 16,
+    color: '#333',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    backgroundColor: '#fff',
+    borderRadius: 25,
+    marginVertical: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 4,
   },
 });
